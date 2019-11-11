2f4cbfe7598797e940dcdb00f4f82185995d169c
jmayank1511 <44773097+jmayank1511@users.noreply.github.com>
2019-11-11 08:57:23 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,39 +1,54 @@
-# Author: Mayank
-def createWindow(vertical,filename):
+
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import random
+import pickle
+max_possible_batch_size = 40
+def breakDict(data, name):
+    """
+    This function will break dict into sublists of size : max_possible_batch_size each
+    """
+
+    batches = []
+    for i in data:
+        values = data[i]
+        for i in range(0,len(values)-1,max_possible_batch_size):
+            if(i+max_possible_batch_size<=len(values)):
+                batches.append(values[i:i+max_possible_batch_size])
+    batches_shuffled = random.sample(batches, len(batches)) 
+    print(name,len(batches_shuffled))
+    with open(name,"wb")as o:
+        pickle.dump(batches_shuffled, o, pickle.HIGHEST_PROTOCOL)
+
+def filterDicts(data,name):
+    """
+    This function removes all the keys with values of len < max_possible_batch__size
+    """
+
+    l = []
+    for i in data:
+        if(len(data[i])<max_possible_batch_size):
+            l.append(i)
+    for i in l:
+        del data[i]
+    breakDict(data,name)
+
+
+
+
+def createWindow(vertical,filename,dress):
     """
     creates a file: filename that contains data in form
     wordi-1 tagi-1 wordi tagi wordi+1 tagi+1 centralWord_id sentID
     this is the intermediate file to be used by findSim() function
     """
-
-    sentId=0
-    dress = ""
-    if (vertical == "dress"):
-        dress = f.readlines()
-    else:
-        dress= g.readlines()
-    wordList = ["$UNK$"]
-    tagList = ['O']
-    for idx,line in enumerate(dress):
-        line = line.strip().split()
-        if(len(line)<2):
-            if(len(wordList)>2):
-                wordList.append("$UNK$")
-                tagList.append("O")
-                for i in range(1,len(wordList)-1):
-                    filename.write(wordList[i-1]+" " +tagList[i-1]+" "+wordList[i]+" " +tagList[i]+" "+wordList[i+1]+" "+tagList[i+1]+" "+str(i-1)+" "+str(sentId)+"\n")
-            sentId+=1
-            wordList = ["$UNK$"]
-            tagList = ['O']
-        else:
-            wordList.append(line[0])
-            if(line[1]=='O'):
-                tagList.append(line[1])
-            else:
-                tagList.append(line[1]+"_"+vertical)
+    for sentno,line in enumerate(dress):
+        line = line.strip().split("\n")
+        for i in range(0,len(line)-2):
+            toprint = line[i]+" " +line[i+1]+" "+line[i+2]+" "+str((i+1))+" "+str(sentno)
+            filename.write(toprint+"\n")
 
 def loadGloveModel(gloveFile):
-    import numpy as np
     print ("Loading Glove Model")
     with open(gloveFile, encoding="utf8" ) as f:
        content = f.readlines()
@@ -46,6 +61,31 @@ def loadGloveModel(gloveFile):
     print ("Done.",len(model)," words loaded!")
     return model
 
+def createLambdaLists(cosine_8,dress_list,jean_list):
+    dress_lambda = {}
+    jean_lambda = {}
+    import copy
+    print(len(cosine_8[0]))
+    for idx,i in enumerate(cosine_8[0]):
+        j = cosine_8[1][idx]
+        x = dress_list[i]
+        x.extend(jean_list[j])
+        if(i in dress_lambda):
+            dress_lambda[i].append(x)
+        else:
+            dress_lambda[i] = [x]
+        if(j in jean_lambda):
+            jean_lambda[j].append(x)
+        else:
+            jean_lambda[j] = [x]
+    print("filtering...")
+    filterDicts(dress_lambda,"data/dress_lambda.pkl")
+    filterDicts(jean_lambda,"data/jean_lambda.pkl")
+    
+
+
+
+
 def findSim():
     """
     finds cosine similarity of word vectors of window size 3 
@@ -55,19 +95,22 @@ def findSim():
     """
     f1 = open('data/sentences_dress.txt','r')
     g1 = open('data/sentences_jean.txt','r')
-    import numpy as np
     zeros = np.zeros(300)
-    jeansVectors = []
     jeans = g1.readlines()
-    #print("#Examples in jean",len(jeans))
-    jean_window_vectors = {} # key: (sentno,centralWordPos); Value = wordvector of window 3
-    dress_window_vectors = {}
-    for line in jeans:
-        line = line.strip().split(" ") 
+    jean_window_vectors = [] # key: (sentno,centralWordPos,tag); Value = wordvector of window 3
+    jean_list = []
+    dress_window_vectors = []
+    dress_list = []
+    for line in jeans:  # line is :  word tag centerword centertag word tag centerwordpos sentno
+        line = line.strip().split(" ")
+        centralTag =  line[3].strip()
+        if(centralTag =='O' or len(line)!=8):
+            continue
         sentNo = int(line[7].strip())
         centralWordPos = int(line[6].strip())
+        if(js[sentNo]<centralWordPos):
+            print("ouch")
         if(line[0].lower() in model):
-
             vector = model[line[0].lower()]
         else:
             vector = zeros
@@ -83,26 +126,24 @@ def findSim():
             vector = np.concatenate((vector,model[line[4].lower()]))
         else:
             vector= np.concatenate((vector,zeros))
-        jean_window_vectors[(sentNo,centralWordPos)]=vector
-        jeansVectors.append(vector)
-    import pickle
-    with open("data/windowVectors_jean.pkl","wb")as o:
-        pickle.dump(jean_window_vectors,o,pickle.HIGHEST_PROTOCOL)
+        jean_list.append([sentNo,centralWordPos,centralTag,vector])        
+        jean_window_vectors.append(vector)
     f1.close()
     g1.close()
-    out = open("data/windowLambda_dressJean.txt",'w')
     zeros = np.zeros(300)
     f1 = open('data/sentences_dress.txt','r')
     dress = f1.readlines()
     g1 = open('data/sentences_jean.txt','r')
-
     jean = g1.readlines()
     for idx,line in enumerate(dress):
-        print(idx)
-        data =line
         line = line.strip().split(" ") 
+        centralTag =  line[3].strip()
+        if(centralTag =='O' or len(line)!=8):
+            continue
         sentNo = int(line[7].strip())
         centralWordPos = int(line[6].strip())
+        if(ds[sentNo]<centralWordPos):
+            print("ouch")
         if(line[0].lower() in model):
             dress_vector = model[line[0].lower()]
         else:
@@ -115,40 +156,26 @@ def findSim():
             dress_vector = np.concatenate((dress_vector,model[line[4].lower()]))
         else:
             dress_vector= np.concatenate((dress_vector,zeros))
-        dress_window_vectors[(sentNo,centralWordPos)]=dress_vector
-        for idx,jeans_vector in enumerate(jeansVectors):
-            dress_norm = np.linalg.norm(dress_vector)
-            jean_norm  = np.linalg.norm(jeans_vector)
-            denominator = dress_norm * jean_norm
-            if(denominator==0):
-                dot=0.0
-            else:
-                dot = np.dot(jeans_vector,dress_vector)/(denominator)
-            out.write(data.strip()+" "+jean[idx].strip()+"\t"+str(dot)+"\n" )
-    import pickle
-    with open("data/windowVectors_dress.pkl","wb")as o:
-        pickle.dump(dress_window_vectors,o,pickle.HIGHEST_PROTOCOL)
+        dress_list.append([sentNo,centralWordPos,centralTag,dress_vector])        
+        dress_window_vectors.append(dress_vector)
+    dress_window_vectors_np = np.array(dress_window_vectors)
+    jean_window_vectors_np = np.array(jean_window_vectors)
+    cosine_sim = cosine_similarity(dress_window_vectors_np,jean_window_vectors_np)
+    cosine_8 = np.nonzero(cosine_sim>0.69)  #creates a tuple ([x cordinate],[y cordinate])
+    createLambdaLists( cosine_8,dress_list,jean_list)
 
-def sort_data():
-    """
-    sorts data of findSim() on cosine_sim and save that 
-    in window_lambda_dressJean_sorted.pkl file
-    """
-    print("SORTING_STARTED")
-    f = open("data/windowLambda_dressJean.txt")
-    data = f.readlines()
-    data.sort(key = lambda x: float(x.strip().split("\t")[1]),reverse=True)
-    import pickle
-    with open("data/window_lambda_dressJean_sorted.pkl",'wb') as o:
-        pickle.dump(data,o,pickle.HIGHEST_PROTOCOL)
-    print("SORTING_COMPLETE")
+        
+
+
+
+
 
 def createLists():
     """
     creates 2 separate pkl files that contain list of sentences of individual tasks
     """
-    dress = open("data/dress_3_45_train.txt").read().split("\n\n")
-    jean  = open("data/jean_3_45_train.txt").read().split("\n\n")
+    dress = open("data/dress_3_45_train.txt").read().split("\n \n")
+    jean  = open("data/jean_3_45_train.txt").read().split("\n \n")
     dress_copy=[]
     for idx,sent in enumerate(dress):
         str=""
@@ -182,6 +209,7 @@ def createLists():
         pickle.dump(dress,o,pickle.HIGHEST_PROTOCOL)
     with open("data/jean_sent_raw_in_list.pkl",'wb') as o:
         pickle.dump(jean,o,pickle.HIGHEST_PROTOCOL)
+    return dress,jean
 
 def convertTagsFileToPkl(tagFileName):
     file = open(tagFileName,'r').readlines()
@@ -200,21 +228,20 @@ def convertTagsFileToPkl(tagFileName):
 # MAIN                
 from model.config import Config
 config = Config()
-createLists()
-f =open('data/dress_3_45_train.txt','r')
-g =open('data/jean_3_45_train.txt','r')
+dress_raw_list,jean_raw_list = createLists()
+ds = []
+js = []
+for i in dress_raw_list:
+    ds.append(len(i.split("\n"))-1)
+for i in jean_raw_list:
+    js.append(len(i.split("\n"))-1)
+print(len(ds),len(js))
 f1 = open('data/sentences_dress.txt','w')
 g1 = open('data/sentences_jean.txt','w')
-createWindow("dress",f1)
-createWindow("jean",g1)
-f.close()
-g.close()
+createWindow("dress",f1,dress_raw_list)
+createWindow("jean",g1,jean_raw_list) 
 f1.close()
 g1.close()
 model = loadGloveModel(config.filename_glove)
 findSim()
-sort_data()
 convertTagsFileToPkl(config.filename_tags)
-
-
-

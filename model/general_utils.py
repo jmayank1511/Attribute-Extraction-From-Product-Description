4e0acd16dfcb070cbbd6420b4c46d78f2faba8e2
jmayank1511 <44773097+jmayank1511@users.noreply.github.com>
2019-11-11 08:53:36 +0530

    Update general_utils.py

--- model/general_utils.py
+++ model/general_utils.py
@@ -1,140 +1,92 @@
-import time
-import sys
-import logging
-import numpy as np
+from model.data_utils import FKDataset
+from model.ner_model import NERModel
+from model.config import Config
 
 
-def get_logger(filename):
-    """Return a logger instance that writes in filename
+def align_data(data):
+    """Given dict with lists, creates aligned strings
+
+    Adapted from Assignment 3 of CS224N
 
     Args:
-        filename: (string) path to log.txt
+        data: (dict) data["x"] = ["I", "love", "you"]
+              (dict) data["y"] = ["O", "O", "O"]
 
     Returns:
-        logger: (instance of logger)
+        data_aligned: (dict) data_align["x"] = "I love you"
+                           data_align["y"] = "O O    O  "
 
     """
-    logger = logging.getLogger('logger')
-    logger.setLevel(logging.DEBUG)
-    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-    handler = logging.FileHandler(filename)
-    handler.setLevel(logging.DEBUG)
-    handler.setFormatter(logging.Formatter(
-            '%(asctime)s:%(levelname)s: %(message)s'))
-    logging.getLogger().addHandler(handler)
+    spacings = [max([len(seq[i]) for seq in data.values()])
+                for i in range(len(data[list(data.keys())[0]]))]
+    data_aligned = dict()
 
-    return logger
+    # for each entry, create aligned string
+    for key, seq in data.items():
+        str_aligned = ""
+        for token, spacing in zip(seq, spacings):
+            str_aligned += token + " " * (spacing - len(token) + 1)
+
+        data_aligned[key] = str_aligned
+
+    return data_aligned
 
 
-class Progbar(object):
-    """Progbar class copied from keras (https://github.com/fchollet/keras/)
 
-    Displays a progress bar.
-    Small edit : added strict arg to update
-    # Arguments
-        target: Total number of steps expected.
-        interval: Minimum visual progress update interval (in seconds).
+def interactive_shell(model):
+    """Creates interactive shell to play with model
+
+    Args:
+        model: instance of NERModel
+
     """
+    model.logger.info("""
+This is an interactive mode.
+To exit, enter 'exit'.
+You can enter a sentence like
+input> I love Paris""")
 
-    def __init__(self, target, width=30, verbose=1):
-        self.width = width
-        self.target = target
-        self.sum_values = {}
-        self.unique_values = []
-        self.start = time.time()
-        self.total_width = 0
-        self.seen_so_far = 0
-        self.verbose = verbose
+    while True:
+        try:
+            # for python 2
+            sentence = raw_input("input> ")
+        except NameError:
+            # for python 3
+            sentence = input("input> ")
 
-    def update(self, current, values=[], exact=[], strict=[]):
-        """
-        Updates the progress bar.
-        # Arguments
-            current: Index of current step.
-            values: List of tuples (name, value_for_last_step).
-                The progress bar will display averages for these values.
-            exact: List of tuples (name, value_for_last_step).
-                The progress bar will display these values directly.
-        """
+        words_raw = sentence.strip().split(" ")
 
-        for k, v in values:
-            if k not in self.sum_values:
-                self.sum_values[k] = [v * (current - self.seen_so_far),
-                                      current - self.seen_so_far]
-                self.unique_values.append(k)
-            else:
-                self.sum_values[k][0] += v * (current - self.seen_so_far)
-                self.sum_values[k][1] += (current - self.seen_so_far)
-        for k, v in exact:
-            if k not in self.sum_values:
-                self.unique_values.append(k)
-            self.sum_values[k] = [v, 1]
+        if words_raw == ["exit"]:
+            break
 
-        for k, v in strict:
-            if k not in self.sum_values:
-                self.unique_values.append(k)
-            self.sum_values[k] = v
+        preds = model.predict(words_raw)
+        to_print = align_data({"input": words_raw, "output": preds})
 
-        self.seen_so_far = current
-
-        now = time.time()
-        if self.verbose == 1:
-            prev_total_width = self.total_width
-            sys.stdout.write("\b" * prev_total_width)
-            sys.stdout.write("\r")
-
-            numdigits = int(np.floor(np.log10(self.target))) + 1
-            barstr = '%%%dd/%%%dd [' % (numdigits, numdigits)
-            bar = barstr % (current, self.target)
-            prog = float(current)/self.target
-            prog_width = int(self.width*prog)
-            if prog_width > 0:
-                bar += ('='*(prog_width-1))
-                if current < self.target:
-                    bar += '>'
-                else:
-                    bar += '='
-            bar += ('.'*(self.width-prog_width))
-            bar += ']'
-            sys.stdout.write(bar)
-            self.total_width = len(bar)
-
-            if current:
-                time_per_unit = (now - self.start) / current
-            else:
-                time_per_unit = 0
-            eta = time_per_unit*(self.target - current)
-            info = ''
-            if current < self.target:
-                info += ' - ETA: %ds' % eta
-            else:
-                info += ' - %ds' % (now - self.start)
-            for k in self.unique_values:
-                if type(self.sum_values[k]) is list:
-                    info += ' - %s: %.4f' % (k,
-                        self.sum_values[k][0] / max(1, self.sum_values[k][1]))
-                else:
-                    info += ' - %s: %s' % (k, self.sum_values[k])
-
-            self.total_width += len(info)
-            if prev_total_width > self.total_width:
-                info += ((prev_total_width-self.total_width) * " ")
-
-            sys.stdout.write(info)
-            sys.stdout.flush()
-
-            if current >= self.target:
-                sys.stdout.write("\n")
-
-        if self.verbose == 2:
-            if current >= self.target:
-                info = '%ds' % (now - self.start)
-                for k in self.unique_values:
-                    info += ' - %s: %.4f' % (k,
-                        self.sum_values[k][0] / max(1, self.sum_values[k][1]))
-                sys.stdout.write(info + "\n")
-
-    def add(self, n, values=[]):
-        self.update(self.seen_so_far+n, values)
+        for key, seq in to_print.items():
+            model.logger.info(seq)
 
 
+def main():
+    # create instance of config
+    config = Config()
+
+    # build model
+    model = NERModel(config)
+    model.build()
+    model.restore_session(config.dir_model)
+
+    # create dataset
+    test1  = FKDataset(config.filename_test1, config.processing_word, config.processing_tag, config.max_iter)
+    test2  = FKDataset(config.filename_test2, config.processing_word, config.processing_tag, config.max_iter)
+
+    # evaluate and interact
+    # evaluate and interact
+    print("testing over task 1")
+    model.evaluate(test1,0)
+    #interactive_shell(model)
+    print("testing over task 2")
+    model.evaluate(test2,1)
+
+
+if __name__ == "__main__":
+    main()
